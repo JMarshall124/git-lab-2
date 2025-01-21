ecaa88c86cbfd5a580e0288b3959df4c6ad78773
Jake Marshall <jm161723@ohio.edu>
2025-01-21 12:05:33 -0500

    Lab 2 program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jake Marshall
+ *        Date: 1/21/2025
+ * Description: Lab 2
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
